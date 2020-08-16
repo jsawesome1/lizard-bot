b96e1eee4c152c870e4c142207bd8e4170a5b645
Jacob Singleton <26128926+jsawesome1@users.noreply.github.com>
2020-08-16 13:38:50 -0400

    Proof of concept for bot

--- bot.lisp
+++ bot.lisp
@@ -1,10 +1,17 @@
 (in-package :lizard-bot)
 
-(defparameter *bot* (make-instance 'glacier:mastodon-bot :config-file "cfg.config"))
+(defun notified (notification)
+  (cond
+    ((glacier:mention-p notification)
+     (let ((status (tooter:status notification)))
+       (glacier:reply status
+		      (format nil "A lizard scuttling from key to key would have scuttled ~a cm to type this toot."
+			      (lizard-length (tooter:content status)))
+		      :visibility (tooter:visibility status))))))
 
-(defun cordial-reply (status)
-  (glacier:reply status (format nil "hi, ~a" (tooter:display-name (tooter:account status)))))
+(defparameter *bot* (make-instance 'glacier:mastodon-bot
+				   :config-file "cfg.config"
+				   :on-notification #'notified))
 
 (defun start-bot ()
-  (glacier:add-command "hello" #'cordial-reply :add-prefix t)
   (glacier:run-bot (*bot*)))
